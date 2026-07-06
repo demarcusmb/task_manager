6a2362c7f6ef52e02f2d52442942a882c24c38f3
demarcusmb <demarcus.m.brown@gmail.com>
2026-07-06 14:41:37 -0500

    Swapped from hard coded data to mongodb

--- express-practice/task-manager/src/controllers/taskController.js
+++ express-practice/task-manager/src/controllers/taskController.js
@@ -1,15 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-// Import the tasks array from the data folder.
-// This acts as our temporary in-memory database.
-const tasks = require("../data/tasks");
-
+const Task = require("../models/Tasks");
 
 // ======================
 // CREATE A NEW TASK
 // ======================
-router.post("/",  (req,res) => {
+router.post("/", async (req,res) => {
     try {
         // Extract the title from the request body
         // Example: { "title": "Learn Express" }
@@ -23,16 +20,10 @@ router.post("/",  (req,res) => {
         }
 
         // Create a new task object
-        const newTask = {
-            // Generate a unique ID using the current timestamp
-            id: Date.now().toString(),
-
+        const newTask = await Task.create ({
             // Store the provided title
             title
-        };
-
-        // Add the new task to the tasks array
-        tasks.push(newTask);
+        });
 
         // Return the newly created task with HTTP status 201 (Created)
         res.status(201).json(newTask);
@@ -48,18 +39,16 @@ router.post("/",  (req,res) => {
 // GET TASK BY ID
 // ======================
 
-router.get("/:id",  (req,res) => {
+router.get("/:id", async (req,res) => {
     try{
     // Search for a task whose ID matches the ID in the URL
     // Example: GET /api/tasks/123
-    const task = tasks.find(
-        (task) => task.id === req.params.id
-    );
+    const task = await Task.findById(req.params.id);
 
     // If no task is found, return an error
     if (!task) {
         return res.status(404).json({
-            message: "Task not found",
+            message: "Task123 not found",
         });
     }
 
@@ -76,20 +65,8 @@ router.get("/:id",  (req,res) => {
 // UPDATE TASK
 // ======================
 
-router.put("/:id",  (req,res) => {
+router.put("/:id", async (req,res) => {
     try{
-        // Find the task using the ID from the URL
-        const task = tasks.find(
-            (task) => task.id === req.params.id
-        );
-
-        // If task doesn't exist, return 404
-        if (!task) {
-            return res.status(404).json({
-                message: "Task not found",
-            });
-        }
-
         // Get the updated title from the request body
         const { title } = req.body;
 
@@ -101,43 +78,50 @@ router.put("/:id",  (req,res) => {
         }
 
         // Update the task title
-        task.title = title;
+        const task = await Task.findByIdAndUpdate(
+           req.params.id,
+            { title },
+            {
+                new: true, // Return updated document
+                runValidators: true // applies schema validation
+            }
+        );
+
+        //Checks if task exist if not returns not found
+        if (!task) {
+            return res.status(404).json({
+                message: "Task not found",
+            });
+        }
 
         // Return the updated task
         res.status(200).json(task);
 
     } catch (error) {
         // Forward errors to the global error handler
-        res.status(400).json(error);
+        res.status(500).json(error);
     }
 });
 
 // ======================
 // DELETE TASK
 // ======================
-router.delete("/:id",  (req, res) => {
+router.delete("/:id", async (req, res) => {
     try {
         // Find the index of the task in the array
-        const index = tasks.findIndex(
-            (task) => task.id === req.params.id
-        );
+        const task = await Task.findByIdAndDelete(req.params.id);
 
-        // If task isn't found, findIndex returns -1
-        if (index === -1) {
+        if(!task) {
             return res.status(404).json({
-                message: "Task not found",
+                message:"Task not found"
             });
         }
-
-        // Remove the task from the array
-        // splice(startIndex, numberOfItemsToRemove)
-        tasks.splice(index, 1);
 
         // Return 204 (No Content) to indicate successful deletion
         res.status(204).send();
 
     } catch (error) {
         // Pass any unexpected errors to the error handler
-        res.status(400).json(error);
+        res.status(500).json(error);
     }
 });
